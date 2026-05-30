a9d34fef877db2ac57b41e7a11dfd47fd0096a89
khimsokcheng165-jpg <khimsokcheng165@gmail.com>
2026-05-30 12:54:21 +0700

    abc

---
+++ PleaseProvideControllerClassName.java
@@ -0,0 +1,28 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button handleCalculate;
+
+    @FXML
+    private Label lblX1;
+
+    @FXML
+    private Label lblX2;
+
+    @FXML
+    private TextField txtA;
+
+    @FXML
+    private TextField txtB;
+
+    @FXML
+    private TextField txtC;
+
+}
+
