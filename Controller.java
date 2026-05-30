b4ea06d9bee8c98b7cef17c7d6e2bba1a1d6a34c
khimsokcheng165-jpg <khimsokcheng165@gmail.com>
2026-05-30 13:28:11 +0700

    Revert "Merge branch 'main' of https://github.com/chanvanchho987-rgb/Calculator001"
    This reverts commit 9c3f4b1d554c2e4f93321172ca7f2aa78124d799, reversing
    changes made to b165412cadbd1f29257161709fbba05f90568de2.

--- Controller.java
+++ Controller.java
@@ -45,38 +45,3 @@ public class Controller {
     }
 
 }
-void calculateRoots(ActionEvent event) {
-        try {
-            double a = Double.parseDouble(txtA.getText());
-            double b = Double.parseDouble(txtB.getText());
-            double c = Double.parseDouble(txtC.getText());
-
-            if (a == 0) {
-                lblX1.setText("x_1 = Invalid (A cannot be 0)");
-                lblX2.setText("");
-                return;
-            }
-
-            double discriminant = (b * b) - (4 * a * c);
-
-            if (discriminant > 0) {
-                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-                lblX1.setText(String.format("x_1 = %.2f", x1));
-                lblX2.setText(String.format("x_2 = %.2f", x2));
-            } else if (discriminant == 0) {
-                double x = -b / (2 * a);
-                lblX1.setText(String.format("x_1 = %.2f", x));
-                lblX2.setText("x_2 = (Same as x_1)");
-            } else {
-                double realPart = -b / (2 * a);
-                double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
-                lblX1.setText(String.format("x_1 = %.2f + %.2fi", realPart, imaginaryPart));
-                lblX2.setText(String.format("x_2 = %.2f - %.2fi", realPart, imaginaryPart));
-            }
-        } catch (NumberFormatException e) {
-            lblX1.setText("Error: Enter valid numbers.");
-            lblX2.setText("");
-        }
-    }
-}
