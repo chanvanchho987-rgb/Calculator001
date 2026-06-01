e3a83359643f14ad5b528b56f20cf05a28dc3067
sva <chanvanchho987@gmail.com>
2026-06-01 23:28:27 +0700

    u

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
     }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("View.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+
+        stage.setTitle("Calculator");
+
+        stage.getIcons().add(new javafx.scene.image.Image(getClass().getResourceAsStream("Photo/itc-cambodia.jpg")));
+
+        stage.show();
+    }
+
 }
